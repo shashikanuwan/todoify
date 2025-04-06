2c1eb6b444206c5dc7c1f853ab2d2e3ed49649df
shashikanuwan <kumararanaweera1999@gmail.com>
2025-04-06 16:27:59 +0530

    refactor: format code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,17 @@
 <script setup>
 import Navigation from '@/components/Navigation.vue'
-import {useRoute} from "vue-router";
-import {useAuthStore} from "@/stores/auth.js";
-import {watch} from "vue";
+import { useRoute } from 'vue-router'
+import { useAuthStore } from '@/stores/auth.js'
+import { watch } from 'vue'
 
 const route = useRoute()
 const authStore = useAuthStore()
 
 watch(
-    () => route.path,
-    () => {
-      authStore.clearMessages()
-    }
+  () => route.path,
+  () => {
+    authStore.clearMessages()
+  },
 )
 </script>
 
